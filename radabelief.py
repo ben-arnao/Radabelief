061f41f9b93da801185289399f836c8611417afd
Ben Arnao <arnaoben@gmail.com>
2020-10-22 17:53:13 -0400

    Update radabelief.py

--- radabelief.py
+++ radabelief.py
@@ -3,6 +3,7 @@ from tensorflow_addons.utils.types import FloatTensorLike
 
 from typing import Union, Callable, Dict
 from typeguard import typechecked
+import numpy as np
 
 
 @tf.keras.utils.register_keras_serializable(package="Addons")
@@ -10,50 +11,28 @@ class RadaBelief(tf.keras.optimizers.Optimizer):
 
     @typechecked
     def __init__(
-        self,
-        learning_rate: Union[FloatTensorLike, Callable, Dict] = 0.001,
-        beta_1: FloatTensorLike = 0.9,
-        beta_2: FloatTensorLike = 0.999,
-        epsilon: FloatTensorLike = 1e-12,
-        weight_decay: Union[FloatTensorLike, Callable, Dict] = 0.0,
-        amsgrad: bool = False,
-        sma_threshold: FloatTensorLike = 5.0,
-        total_steps: int = 0,
-        warmup_proportion: FloatTensorLike = 0.1,
-        min_lr: FloatTensorLike = 0.0,
-        name: str = "RectifiedAdam",
-        **kwargs
+            self,
+            learning_rate: Union[FloatTensorLike, Callable, Dict] = 1e-4,
+            beta_1: FloatTensorLike = 0.9,
+            beta_2: FloatTensorLike = 0.999,
+            epsilon: FloatTensorLike = 1e-12,
+            warmup_steps: int = 10000,
+            name: str = "Radabelief",
+            **kwargs
     ):
         super().__init__(name, **kwargs)
-
-        if isinstance(learning_rate, Dict):
-            learning_rate = tf.keras.optimizers.schedules.deserialize(learning_rate)
-
-        if isinstance(weight_decay, Dict):
-            weight_decay = tf.keras.optimizers.schedules.deserialize(weight_decay)
 
         self._set_hyper("learning_rate", kwargs.get("lr", learning_rate))
         self._set_hyper("beta_1", beta_1)
         self._set_hyper("beta_2", beta_2)
-        self._set_hyper("decay", self._initial_decay)
-        self._set_hyper("weight_decay", weight_decay)
-        self._set_hyper("sma_threshold", sma_threshold)
-        self._set_hyper("total_steps", int(total_steps))
-        self._set_hyper("warmup_proportion", warmup_proportion)
-        self._set_hyper("min_lr", min_lr)
+        self._set_hyper("warmup_steps", warmup_steps)
         self.epsilon = epsilon or tf.keras.backend.epsilon()
-        self.amsgrad = amsgrad
-        self._has_weight_decay = weight_decay != 0.0
-        self._initial_total_steps = total_steps
 
     def _create_slots(self, var_list):
         for var in var_list:
             self.add_slot(var, "m")
         for var in var_list:
             self.add_slot(var, "v")
-        if self.amsgrad:
-            for var in var_list:
-                self.add_slot(var, "vhat")
 
     def set_weights(self, weights):
         params = self.weights
@@ -62,155 +41,67 @@ class RadaBelief(tf.keras.optimizers.Optimizer):
             weights = weights[: len(params)]
         super().set_weights(weights)
 
-    def _decayed_wd(self, var_dtype):
-        wd_t = self._get_hyper("weight_decay", var_dtype)
-        if isinstance(wd_t, tf.keras.optimizers.schedules.LearningRateSchedule):
-            wd_t = tf.cast(wd_t(self.iterations), var_dtype)
-        return wd_t
-
     def _resource_apply_dense(self, grad, var):
         var_dtype = var.dtype.base_dtype
-        lr_t = self._decayed_lr(var_dtype)
-        wd_t = self._decayed_wd(var_dtype)
+
+        # *current* learn rate of optimizer (changed by ReduceLROnPlateau for example)
+        lr_t = self.lr
+
+        # get previous moments
         m = self.get_slot(var, "m")
         v = self.get_slot(var, "v")
-        beta_1_t = self._get_hyper("beta_1", var_dtype)
-        beta_2_t = self._get_hyper("beta_2", var_dtype)
+
+        # static hyperparameters of optimizer
+        lr_0 = self._get_hyper("learning_rate", var_dtype)
+        warmup_steps = self._get_hyper("warmup_steps", var_dtype)
+        beta_1 = self._get_hyper("beta_1", var_dtype)
+        beta_2 = self._get_hyper("beta_2", var_dtype)
+
+        # smaller epsilon == more bias == more like SGD
+        # larger epsilon == more adaptive, potential for large LR difference between variables
         epsilon_t = tf.convert_to_tensor(self.epsilon, var_dtype)
+
+        # current step of optimizer
         local_step = tf.cast(self.iterations + 1, var_dtype)
-        beta_1_power = tf.pow(beta_1_t, local_step)
-        beta_2_power = tf.pow(beta_2_t, local_step)
 
-        if self._initial_total_steps > 0:
-            total_steps = self._get_hyper("total_steps", var_dtype)
-            warmup_steps = total_steps * self._get_hyper("warmup_proportion", var_dtype)
-            min_lr = self._get_hyper("min_lr", var_dtype)
-            decay_steps = tf.maximum(total_steps - warmup_steps, 1)
-            decay_rate = (min_lr - lr_t) / decay_steps
-            lr_t = tf.where(
-                local_step <= warmup_steps,
-                lr_t * (local_step / warmup_steps),
-                lr_t + decay_rate * tf.minimum(local_step - warmup_steps, decay_steps),
-            )
+        # precalculate betas to power of iters
+        beta_1_power = tf.pow(beta_1, local_step)
+        beta_2_power = tf.pow(beta_2, local_step)
 
-        sma_inf = 2.0 / (1.0 - beta_2_t) - 1.0
-        sma_t = sma_inf - 2.0 * local_step * beta_2_power / (1.0 - beta_2_power)
-
-        m_t = m.assign(
-            beta_1_t * m + (1.0 - beta_1_t) * grad, use_locking=self._use_locking
+        # use linearly scaled lr from 0 to initial LR while steps under warmup_steps
+        # when out of warmup, use regular LR
+        lr = tf.where(
+            local_step <= warmup_steps,
+            (local_step / warmup_steps) * lr_0,
+            lr_t,
         )
-        m_corr_t = m_t / (1.0 - beta_1_power)
 
+        # calculate first moment of gradient (momemtum)
+        m_t = m.assign(
+            beta_1 * m + (1.0 - beta_1) * grad,
+            use_locking=self._use_locking
+        )
+
+        # calculate second moment of gradient (RMSprop)
+        # use 'tf.square(grad - m_t)' for Adabelief instead of 'tf.square(grad)'
         v_t = v.assign(
-            beta_2_t * v + (1.0 - beta_2_t) * tf.square(grad - m_t),  # with adabelief
-            # beta_2_t * v + (1.0 - beta_2_t) * tf.square(grad),  # without adabelief
+            beta_2 * v + (1.0 - beta_2) * tf.square(grad - m_t),
             use_locking=self._use_locking,
         )
-        if self.amsgrad:
-            vhat = self.get_slot(var, "vhat")
-            vhat_t = vhat.assign(tf.maximum(vhat, v_t), use_locking=self._use_locking)
-            v_corr_t = tf.sqrt(vhat_t / (1.0 - beta_2_power))
-        else:
-            vhat_t = None
-            v_corr_t = tf.sqrt(v_t / (1.0 - beta_2_power))
 
-        r_t = tf.sqrt(
-            (sma_t - 4.0)
-            / (sma_inf - 4.0)
-            * (sma_t - 2.0)
-            / (sma_inf - 2.0)
-            * sma_inf
-            / sma_t
-        )
-
-        sma_threshold = self._get_hyper("sma_threshold", var_dtype)
-        var_t = tf.where(
-            sma_t >= sma_threshold, r_t * m_corr_t / (v_corr_t + epsilon_t), m_corr_t
-        )
-
-        if self._has_weight_decay:
-            var_t += wd_t * var
-
-        var_update = var.assign_sub(lr_t * var_t, use_locking=self._use_locking)
-
-        updates = [var_update, m_t, v_t]
-        if self.amsgrad:
-            updates.append(vhat_t)
-        return tf.group(*updates)
-
-    def _resource_apply_sparse(self, grad, var, indices):
-        var_dtype = var.dtype.base_dtype
-        lr_t = self._decayed_lr(var_dtype)
-        wd_t = self._decayed_wd(var_dtype)
-        beta_1_t = self._get_hyper("beta_1", var_dtype)
-        beta_2_t = self._get_hyper("beta_2", var_dtype)
-        epsilon_t = tf.convert_to_tensor(self.epsilon, var_dtype)
-        local_step = tf.cast(self.iterations + 1, var_dtype)
-        beta_1_power = tf.pow(beta_1_t, local_step)
-        beta_2_power = tf.pow(beta_2_t, local_step)
-
-        if self._initial_total_steps > 0:
-            total_steps = self._get_hyper("total_steps", var_dtype)
-            warmup_steps = total_steps * self._get_hyper("warmup_proportion", var_dtype)
-            min_lr = self._get_hyper("min_lr", var_dtype)
-            decay_steps = tf.maximum(total_steps - warmup_steps, 1)
-            decay_rate = (min_lr - lr_t) / decay_steps
-            lr_t = tf.where(
-                local_step <= warmup_steps,
-                lr_t * (local_step / warmup_steps),
-                lr_t + decay_rate * tf.minimum(local_step - warmup_steps, decay_steps),
-            )
-
-        sma_inf = 2.0 / (1.0 - beta_2_t) - 1.0
-        sma_t = sma_inf - 2.0 * local_step * beta_2_power / (1.0 - beta_2_power)
-
-        m = self.get_slot(var, "m")
-        m_scaled_g_values = grad * (1 - beta_1_t)
-        m_t = m.assign(m * beta_1_t, use_locking=self._use_locking)
-        with tf.control_dependencies([m_t]):
-            m_t = self._resource_scatter_add(m, indices, m_scaled_g_values)
+        # correct bias (mostly affects initial steps in which gradient will tend to be closer to beta,
+        # as opposed to 0 (neutral)
         m_corr_t = m_t / (1.0 - beta_1_power)
+        v_corr_t = v_t / (1.0 - beta_2_power)
 
-        v = self.get_slot(var, "v")
-        # v_scaled_g_values = (grad * grad) * (1 - beta_2_t)
-        v_scaled_g_values = (grad - m_t) * (grad - m_t) * (1 - beta_2_t)
-        v_t = v.assign(v * beta_2_t, use_locking=self._use_locking)
-        with tf.control_dependencies([v_t]):
-            v_t = self._resource_scatter_add(v, indices, v_scaled_g_values)
+        # calculate step
+        var_t = m_corr_t / (tf.sqrt(v_corr_t) + epsilon_t)
 
-        if self.amsgrad:
-            vhat = self.get_slot(var, "vhat")
-            vhat_t = vhat.assign(tf.maximum(vhat, v_t), use_locking=self._use_locking)
-            v_corr_t = tf.sqrt(vhat_t / (1.0 - beta_2_power))
-        else:
-            vhat_t = None
-            v_corr_t = tf.sqrt(v_t / (1.0 - beta_2_power))
-
-        r_t = tf.sqrt(
-            (sma_t - 4.0)
-            / (sma_inf - 4.0)
-            * (sma_t - 2.0)
-            / (sma_inf - 2.0)
-            * sma_inf
-            / sma_t
-        )
-
-        sma_threshold = self._get_hyper("sma_threshold", var_dtype)
-        var_t = tf.where(
-            sma_t >= sma_threshold, r_t * m_corr_t / (v_corr_t + epsilon_t), m_corr_t
-        )
-
-        if self._has_weight_decay:
-            var_t += wd_t * var
-
-        with tf.control_dependencies([var_t]):
-            var_update = self._resource_scatter_add(
-                var, indices, tf.gather(-lr_t * var_t, indices)
-            )
+        # apply learn rate
+        var_update = var.assign_sub(lr * var_t,
+                                    use_locking=self._use_locking)
 
         updates = [var_update, m_t, v_t]
-        if self.amsgrad:
-            updates.append(vhat_t)
         return tf.group(*updates)
 
     def get_config(self):
@@ -220,16 +111,71 @@ class RadaBelief(tf.keras.optimizers.Optimizer):
                 "learning_rate": self._serialize_hyperparameter("learning_rate"),
                 "beta_1": self._serialize_hyperparameter("beta_1"),
                 "beta_2": self._serialize_hyperparameter("beta_2"),
-                "decay": self._serialize_hyperparameter("decay"),
-                "weight_decay": self._serialize_hyperparameter("weight_decay"),
-                "sma_threshold": self._serialize_hyperparameter("sma_threshold"),
                 "epsilon": self.epsilon,
-                "amsgrad": self.amsgrad,
-                "total_steps": self._serialize_hyperparameter("total_steps"),
-                "warmup_proportion": self._serialize_hyperparameter(
-                    "warmup_proportion"
-                ),
-                "min_lr": self._serialize_hyperparameter("min_lr"),
+                "warmup_steps": self._serialize_hyperparameter("warmup_steps"),
             }
         )
         return config
+
+
+class ReduceLROnPlateau(tf.keras.callbacks.Callback):
+    def __init__(self,
+                 monitor='loss',
+                 factor=0.1,
+                 patience=10,
+                 verbose=0,
+                 min_delta=1e-4,
+                 cooldown=0,
+                 min_lr=0):
+        super(ReduceLROnPlateau, self).__init__()
+
+        self.monitor = monitor
+        self.factor = factor
+        self.min_lr = min_lr
+        self.min_delta = min_delta
+        self.patience = patience
+        self.verbose = verbose
+        self.cooldown = cooldown
+        self.cooldown_counter = 0  # Cooldown counter.
+        self.wait = 0
+        self.best = 0
+        self.monitor_op = None
+        self._reset()
+
+    def _reset(self):
+        self.monitor_op = lambda a, b: np.less(a, b - self.min_delta)
+        self.best = np.Inf
+        self.cooldown_counter = 0
+        self.wait = 0
+
+    def on_train_begin(self, logs=None):
+        self._reset()
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        logs['lr'] = self.model.optimizer.lr
+        current = logs.get(self.monitor)
+
+        if self.in_cooldown():
+            self.cooldown_counter -= 1
+            self.wait = 0
+
+        if self.monitor_op(current, self.best):
+            self.best = current
+            self.wait = 0
+        elif not self.in_cooldown():
+            self.wait += 1
+            if self.wait >= self.patience:
+                old_lr = self.model.optimizer.lr
+                if old_lr > np.float32(self.min_lr):
+                    new_lr = old_lr * self.factor
+                    new_lr = max(new_lr, self.min_lr)
+                    self.model.optimizer.lr = new_lr
+                    if self.verbose > 0:
+                        print('\nEpoch %05d: ReduceLROnPlateau reducing learning '
+                              'rate to %s.' % (epoch + 1, new_lr.numpy()))
+                    self.cooldown_counter = self.cooldown
+                    self.wait = 0
+
+    def in_cooldown(self):
+        return self.cooldown_counter > 0
